6099d55d9a3e8d078f00caf671816d38e767f8c3
Lampadare <ml2419@ic.ac.uk>
2024-07-24 20:35:47 +0100

    intan working i believe

--- build/image_preload.cmake
+++ build/image_preload.cmake
@@ -1,6 +1,6 @@
 # Generated file that can be used to preload variant images
 set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "/Users/Martin/Desktop/HBR/Masters/MARM_fmw_v0;/opt/nordic/ncs/v2.6.1/nrf;/opt/nordic/ncs/v2.6.1/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "/Users/Martin/Desktop/HBR/Masters/MARM_fmw_v0;/Users/Martin/Desktop/HBR/Masters/fmw_tests/intan_tests;/opt/nordic/ncs/v2.6.1/nrf;/opt/nordic/ncs/v2.6.1/zephyr" CACHE INTERNAL "NCS child image controlled")
 set(CACHED_CONF_FILE "/Users/Martin/Desktop/HBR/Masters/MARM_fmw_v0/prj.conf" CACHE INTERNAL "NCS child image controlled")
 set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
 set(DTC_OVERLAY_FILE "/Users/Martin/Desktop/HBR/Masters/MARM_fmw_v0/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
